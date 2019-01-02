8868d265eef3df03e190d64f87f184a304377df0
nguye <nguyenv2@outlook.com>
2019-01-02 12:19:43 -0800

    use API for anagrams instead of function

--- AnagramSolver/AnagramSolver/views.py
+++ AnagramSolver/AnagramSolver/views.py
@@ -6,9 +6,11 @@ from datetime import datetime
 from flask import render_template
 from AnagramSolver import app
 from flask import Flask, request
+import json
+import requests
 
 	#09e0e9b5
-
+#gets all possible permutations
 def find_anagram(word):
   if len(word) <= 1:
     return word
@@ -16,7 +18,7 @@ def find_anagram(word):
     word_array = []
     for anagram in find_anagram(word[1:]):
       for i in range(len(word)):
-        word_array.append(anagram[:i]+word[0:1] + anagram[i:])
+        word_array.append(anagram[:i] + word[0:1] + anagram[i:])
     return word_array
 
 @app.route('/')
@@ -30,8 +32,10 @@ def home():
 @app.route('/anagram', methods=['POST'])
 def anagram():
   word = request.form['getWord']
-  get_anagram = find_anagram(word)
+  stringURL = 'http://www.anagramica.com/best/' + word
+  getJSON = requests.get(stringURL)
+  anagram = json.loads(getJSON.text)
   return render_template('index.html',
-    title='Home Page',
-    anagrams = get_anagram,
-    year=datetime.now().year,)
+    title = 'Home Page',
+    year = datetime.now().year,
+    anagrams = anagram['best'])
