75ec52db4c24f9c3dcb88b682128646db4b5366f
nguye <nguyenv2@outlook.com>
2019-01-02 13:52:07 -0800

    need better anagram api

--- AnagramSolver/AnagramSolver/views.py
+++ AnagramSolver/AnagramSolver/views.py
@@ -9,7 +9,11 @@ from flask import Flask, request
 import json
 import requests
 
-	#09e0e9b5
+class Word():
+  def __init__(self, word, definition):
+    self.word = word
+    self.definition = definition
+
 #gets all possible permutations
 def find_anagram(word):
   if len(word) <= 1:
@@ -32,10 +36,29 @@ def home():
 @app.route('/anagram', methods=['POST'])
 def anagram():
   word = request.form['getWord']
+  print(word)
   stringURL = 'http://www.anagramica.com/best/' + word
   getJSON = requests.get(stringURL)
   anagram = json.loads(getJSON.text)
+  wordList = []
+
+  for eachword in anagram['best']:
+    wordList.append(Word(eachword, dictionary(eachword)))
   return render_template('index.html',
     title = 'Home Page',
     year = datetime.now().year,
-    anagrams = anagram['best'])
+    anagrams = wordList)
+
+def dictionary(word):
+  app_id = 'hh'
+  app_key = 'll'
+  language = 'en'
+  url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word.lower()
+
+  print(word)
+  response = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+  print(response.text)
+  print("Code {}\n".format(response.status_code))
+  getJSON = json.loads(response.text)
+  definition = getJSON['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
+  return definition
